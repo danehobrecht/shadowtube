2c2e1622798f07414d7b5352e9c457cfe77ce724
tmuq <76x5l22l@anonaddy.me>
2021-11-01 12:39:41 -0700

    modified:   shadowtube.py

--- shadowtube.py
+++ shadowtube.py
@@ -42,17 +42,17 @@ socks_port = settings_dict["socks_port"]
 
 ### Tor
 
-def get_tor_session():
-	session = requests.Session()
-	session.proxies = {"http": "socks5://localhost:" + str(socks_port), "https": "socks5://localhost:" + str(socks_port)}
-	return session
+def tor_authenticate():
+	s = requests.Session()
+	s.proxies = {"http": "socks5://localhost:" + str(socks_port), "https": "socks5://localhost:" + str(socks_port)}
+	return s
 
-def rotate_connection():
+def tor_rotate():
 	time.sleep(10)
 	try:
-		with Controller.from_port(port=9151) as c:
+		with Controller.from_port(port = 9151) as c:
 			if use_control_pass:
-				c.authenticate(password=control_pass)
+				c.authenticate(password = control_pass)
 				c.signal(Signal.NEWNYM)
 			else:
 				c.authenticate()
@@ -61,63 +61,62 @@ def rotate_connection():
 		print("Error: Failed to authenticate. Control port password incorrect.")
 		sys.exit(1)
 	except SocketError:
-		print("Error: Connection refused. Ensure cookie authentication/control port is enabled.")
+		print("Error: Connection refused. Ensure cookie authentication/control port are enabled.")
 		sys.exit(1)
 
-def check_tor():
-	attempts = 0
+def tor_validate():
+	att = 0
 	print("Reaching Tor service...")
 	while True:
 		try:
-			get_tor_session().get("https://ip.seeip.org")
-			print("Successful.")
+			tor_authenticate().get("https://ip.seeip.org")
+			print("Success")
 			time.sleep(1.2)
 			os.system("clear")
 			break
 		except IOError:
-			print("Failed.")
-			print("Trying again in 5 seconds.")
-			attempts += 1
+			print("Error: Failed to reach Tor. Trying again in 5 seconds.")
+			att += 1
 			time.sleep(5)
-			if attempts == 10:
-				print("Error: User idle. Exiting.")
+			if att == 10:
+				print("User idle. Exiting.")
 				sys.exit(1)
 
 ### Output
 
-def geoip():
+def out_geoip():
 	try:
-		r = get_tor_session().get("https://ip.seeip.org/geoip")
+		r = tor_authenticate().get("https://ip.seeip.org/geoip")
 		r_dict = r.json()
 		print(r_dict["country"] + " — " + r_dict["ip"])
 	except IOError:
 		print("Unknown")
 
-def conclusion(attempts, accessible):
-	if attempts == 0:
+def out_conclude(att, acc):
+	if att == 0:
 		print("\nInterrupted before granted sufficient time")
-	elif accessible == 0 and attempts > 0:
+	elif acc == 0 and att > 0:
 		print("\nAlarming behavior detected")
-	elif attempts > accessible:
+	elif att > acc:
 		print("\nQuestionable behavior detected")
-	elif attempts == accessible and attempts > 0:
+	elif att == acc and att > 0:
 		print("\nNo abnormal behavior detected")
 
 ### Video - https://www.youtube.com/watch?v=Y6ljFaKRTrI
 
-def video(youtube_id):
-	attempts = 0
-	accessible = 0
+def video_init(youtube_id):
+	att = 0
+	acc = 0
 	url = "https://www.youtube.com/watch?v=" + youtube_id
 	try:
 		while True:
 			try:
-				page_data = get_tor_session().get(url).text
+				page_data = tor_authenticate().get(url).text
 				parse_title = str(re.findall('<title>(.*?) - YouTube</title><meta name="title" content=', page_data))
 				title = html.unescape(parse_title.split("'")[1])
 				break
 			except IndexError:
-				rotate_connection()
+				tor_rotate()
 		if title == "":
 			print("\nVideo unavailable")
 			sys.exit(1)
@@ -125,27 +124,27 @@ def video(youtube_id):
 			print("\n" + title)
 		print("Interrupt (CTRL+C) to conclude the session\n")
 		while True:
-			rotate_connection()
-			query = get_tor_session().get("https://www.youtube.com/results?search_query=" + "+".join(title.split())).text
-			if query.find('"title":{"runs":[{"text":"') >= 0:
-				if query.find(title) >= 0:
-					accessible += 1
+			tor_rotate()
+			q = tor_authenticate().get("https://www.youtube.com/results?search_query=" + "+".join(title.split())).text
+			if q.find('"title":{"runs":[{"text":"') >= 0:
+				if q.find(title) >= 0:
+					acc += 1
 					print("[✓]", end=" ")
 				else:
 					print("[x]", end=" ")
-				geoip()
-				attempts += 1
-		conclusion(attempts, accessible)
+				out_geoip()
+				att += 1
+		out_conclude(att, acc)
 	except KeyboardInterrupt:
-		conclusion(attempts, accessible)
+		out_conclude(att, acc)
 
 ### Comments - https://www.youtube.com/feed/history/comment_history
 ### Comment url template (removed) - https://www.youtube.com/watch?v=OfsojVaqyAA&lc=Ugx5BtG_-N5pwDyvOiF4AaABAg.9NEWMl2CCJR9NI73GZeCDa
 
-def comments():
-	attempts = 0
-	accessible = 0
-	index = 1
+def comments_init():
+	att = 0
+	acc = 0
+	ind = 1
 	print("\nInterrupt (CTRL+C) to conclude the session")
 	try:
 		with io.open("Google - My Activity.html", "r", encoding = "utf-8") as raw_html:
@@ -154,38 +153,38 @@ def comments():
 			uuid_list = str(re.findall('data-token="(.*?)" data-date', html))
 			url_list = str(re.findall('<div class="iXL6O"><a href="(.*?)" jslog="65086; track:click"', html))
 			for i in range(int(url_list.count("'") / 2)):
-				text = text_list.split("'")[index]
-				uuid = uuid_list.split("'")[index]
-				url = url_list.split("'")[index]
+				text = text_list.split("'")[ind]
+				uuid = uuid_list.split("'")[ind]
+				url = url_list.split("'")[ind]
 				comment_url = url + "&lc=" + uuid
-				instances = 0
-				index += 2
-				print('\n"' +text.replace("`", "'") + '"')
+				ins = 0
+				ind += 2
+				print('\n"' + text.replace("`", "'") + '"')
 				print(url + "\n")
 				for i in range(0, 3, 1):
-					fetch_comments(url.replace("https://www.youtube.com/watch?v=", ""))
-					if private == bool(True):
+					comments_fetch(url.replace("https://www.youtube.com/watch?v=", ""))
+					if private == True:
 						break
 					with open("temp.json", "r") as json:
 						j = json.read()
 						if j.find(uuid) >= 0:
 							print("[✓]", end=" ")
-							instances += 1
+							ins += 1
 						else:
 							print("[x]", end=" ")
-							if instances > 0:
-								instances -= 1
-						geoip()
-					rotate_connection()
-				if private == bool(False):
-					if instances == 3:
-						accessible += 1
+							if ins > 0:
+								ins -= 1
+						out_geoip()
+					tor_rotate()
+				if private == False:
+					if ins == 3:
+						acc += 1
 					attempts += 1
-		conclusion(attempts, accessible)
+		out_conclude(att, acc)
 	except KeyboardInterrupt:
-		conclusion(attempts, accessible)
+		out_conclude(att, acc)
 
-def fetch_comments(youtube_id):
+def comments_fetch(youtube_id):
 	parser = argparse.ArgumentParser()
 	try:
 		args, unknown = parser.parse_known_args()
@@ -199,7 +198,7 @@ def fetch_comments(youtube_id):
 				os.makedirs(outdir)
 		count = 0
 		with io.open(output, 'w', encoding='utf8') as fp:
-			for comment in download_comments(youtube_id):
+			for comment in comments_download(youtube_id):
 				comment_json = json.dumps(comment, ensure_ascii=False)
 				print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
 				count += 1
@@ -224,9 +223,9 @@ def ajax_request(session, url, params=None, data=None, headers=None, retries=5, 
 		else:
 			time.sleep(sleep)
 
-def download_comments(youtube_id, sleep=.1):
+def comments_download(youtube_id, sleep=.1):
 	global private
-	private = bool(False)
+	private = False
 	session = requests.Session()
 	session.headers['User-Agent'] = USER_AGENT
 
@@ -243,10 +242,10 @@ def download_comments(youtube_id, sleep=.1):
 			break
 	try:
 		if not ncd:
-			private = bool(False)
+			private = False
 			return
 	except UnboundLocalError:
-		private = bool(True)
+		private = True
 		print("Private video")
 		return
 	continuations = [(ncd['continuation'], ncd['clickTrackingParams'], 'action_get_comments')]
@@ -308,32 +307,32 @@ def main():
 	group.add_argument("-c", "--comments", help="analyze locally available comment history", action="store_true")
 	args = parser.parse_args()
 	if args.video:
-		check_tor()
+		tor_validate()
 		while True:
-			print("Complete the video URL in question\n")
+			print("Complete the video URL in question")
 			youtube_id = input("https://www.youtube.com/watch?v=")
 			count = 0
 			for c in youtube_id:
 				if c.isspace() != True:
 					count = count + 1
 			if count == 11:
-				response = get_tor_session().get("https://www.youtube.com/watch?v=" + youtube_id)
+				response = tor_authenticate().get("https://www.youtube.com/watch?v=" + youtube_id)
 				break
 			else:
 				os.system("clear")
-		video(youtube_id)
+		video_init(youtube_id)
 	elif args.comments:
-		check_tor()
+		tor_validate()
 		while True:
 			try:
-				print('The basic HTML page data from https://www.youtube.com/feed/history/comment_history must be locally available to the script as "Google - My Activity.html"\n')
+				print('The basic HTML page data from https://www.youtube.com/feed/history/comment_history must be locally available to the script as "Google - My Activity.html"')
 				confirm = input("Confirm? (Y) ")
 				if confirm == "Y" or confirm == "y":
 					try:
 						io.open("Google - My Activity.html", "r")
 						break
 					except IOError:
-						print("Error: File does not exist. Please download the file listed above and place it in the project directory.")
+						print("Error: File does not exist.")
 				elif confirm == "N" or confirm == "n":
 					print("Exiting.")
 					sys.exit(1)
@@ -341,7 +340,7 @@ def main():
 					os.system("clear")
 			except ValueError:
 				continue
-		comments()
+		comments_init()
 	else:
 		os.system("python3 shadowtube.py -h")
 		
